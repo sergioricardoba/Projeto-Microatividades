987f43d3de38a7f2b820d35d4dfa63ea9df39a24
sergioricardoba <sergioricardoba@gmail.com>
2025-11-04 20:02:52 -0300

    Add files via upload
    Microatividade 1: Descrever a utilização das estruturas de condição
    if e else em Python

---
+++ estruturas_condicao1.py
@@ -0,0 +1,10 @@
+# 1. Crie uma variável chamada temperatura e atribua a ela o valor 29
+# 2. Crie uma verificação, utilizando a condição if, para checar se o valor da variável temperatura é menor que 30
+# 3. Caso positivo, imprima na tela o texto ‘A temperatura hoje está amena’
+# 4. Caso contrário, e utilizando a condição else, imprima na tela o texto ‘Hoje está fazendo calor’
+
+temperatura = 31
+if temperatura < 30:
+    print('A temperatura hoje está amena')
+else:
+    print('Hoje está fazendo calor')
