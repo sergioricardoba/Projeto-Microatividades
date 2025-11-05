b87001ac2781c070e01e923f06d9214e1d74a274
sergioricardoba <sergioricardoba@gmail.com>
2025-11-04 21:00:08 -0300

    Add files via upload
    Microatividade 5: Descrever a utilização de funções em Python

---
+++ funcoes1.py
@@ -0,0 +1,10 @@
+# 1. Defina uma função chamada “imprimir_variavel”. Tal instrução não receberá parâmetros;
+# 2. No escopo da função acima, crie uma variável chamada texto e atribua a ela o
+# valor ‘Olá, funções em Python’;
+# 3. A seguir, ainda no escopo da função, imprima na tela o valor da variável texto;
+# 4. Por último, fora do escopo da função, faça a chamada da mesma.
+
+def imprimir_variavel():
+    texto = 'Olá, funções em Python'
+    print(texto)
+imprimir_variavel()
